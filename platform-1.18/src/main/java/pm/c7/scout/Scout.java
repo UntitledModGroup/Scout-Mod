3bbc5a3611774dff2683a8ef2d0adf7e151235aa
Cynthia Foxwell <gamers@riseup.net>
2022-06-13 18:36:13 -0600

    initial mod setup, item registry, trinket slot data

---
+++ platform-1.18/src/main/java/pm/c7/scout/Scout.java
@@ -0,0 +1,29 @@
+package pm.c7.scout;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import pm.c7.scout.item.BaseBagItem;
+
+public class Scout implements ModInitializer {
+    public static final String MOD_ID = "scout";
+
+    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier("scout", "itemgroup"), () -> new ItemStack(Scout.SATCHEL));
+
+    public static final BaseBagItem SATCHEL = new BaseBagItem(new FabricItemSettings().group(Scout.ITEM_GROUP));
+    public static final BaseBagItem UPGRADED_SATCHEL = new BaseBagItem(new FabricItemSettings().group(Scout.ITEM_GROUP));
+    public static final BaseBagItem POUCH = new BaseBagItem(new FabricItemSettings().group(Scout.ITEM_GROUP));
+    public static final BaseBagItem UPGRADED_POUCH = new BaseBagItem(new FabricItemSettings().group(Scout.ITEM_GROUP));
+
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "satchel"), SATCHEL);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "upgraded_satchel"), UPGRADED_SATCHEL);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "pouch"), POUCH);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "upgraded_pouch"), UPGRADED_POUCH);
+    }
+}
---
+++ platform-1.18/src/main/java/pm/c7/scout/item/BaseBagItem.java
@@ -0,0 +1,9 @@
+package pm.c7.scout.item;
+
+import dev.emi.trinkets.api.TrinketItem;
+
+public class BaseBagItem extends TrinketItem {
+    public BaseBagItem(Settings settings) {
+        super(settings);
+    }
+}
