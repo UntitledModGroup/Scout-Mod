4026730eb7db6931321ac1b5d796e1a663230365
Cynthia Foxwell <gamers@riseup.net>
2022-06-18 20:30:41 -0600

    REI support for prosperity

--- platform-1.18/src/main/java/pm/c7/scout/compat/ScoutREIClientPlugin.java
+++ platform-1.18/src/main/java/pm/c7/scout/compat/ScoutREIClientPlugin.java
@@ -1,8 +1,12 @@
 package pm.c7.scout.compat;
 
-import dev.emi.emi.api.EmiPlugin;
-import dev.emi.emi.api.EmiRegistry;
-import dev.emi.emi.api.widget.Bounds;
+import java.util.ArrayList;
+import java.util.List;
+
+import me.shedaniel.math.Rectangle;
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.screen.ExclusionZones;
+import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.item.ItemStack;
@@ -10,11 +14,13 @@ import pm.c7.scout.ScoutUtil;
 import pm.c7.scout.item.BaseBagItem;
 import pm.c7.scout.mixin.HandledScreenAccessor;
 
-public class ScoutEmiPlugin implements EmiPlugin {
-
+public class ScoutREIClientPlugin implements REIClientPlugin {
     @Override
-    public void register(EmiRegistry registry) {
-        registry.addExclusionArea(InventoryScreen.class, (screen, consumer) -> {
+    public void registerScreens(ScreenRegistry registry) {
+        ExclusionZones zones = registry.exclusionZones();
+        zones.register(InventoryScreen.class, screen -> {
+            List<Rectangle> bounds = new ArrayList<>();
+
             MinecraftClient client = MinecraftClient.getInstance();
             ItemStack leftPouchStack = ScoutUtil.getTrinketSlot(client.player, "legs/pouch", 0);
             if (!leftPouchStack.isEmpty()) {
@@ -25,7 +31,7 @@ public class ScoutEmiPlugin implements EmiPlugin {
                 int x = ((HandledScreenAccessor) screen).getX() - (columns * 18);
                 int y = ((HandledScreenAccessor) screen).getY() + 76;
 
-                consumer.accept(new Bounds(x, y, columns * 18, 68));
+                bounds.add(new Rectangle(x, y, columns * 18, 68));
             }
 
             ItemStack rightPouchStack = ScoutUtil.getTrinketSlot(client.player, "legs/pouch", 1);
@@ -37,9 +43,10 @@ public class ScoutEmiPlugin implements EmiPlugin {
                 int x = ((HandledScreenAccessor) screen).getX() + 176;
                 int y = ((HandledScreenAccessor) screen).getY() + 76;
 
-                consumer.accept(new Bounds(x, y, columns * 18, 68));
+                bounds.add(new Rectangle(x, y, columns * 18, 68));
             }
+
+            return bounds;
         });
     }
-
 }
