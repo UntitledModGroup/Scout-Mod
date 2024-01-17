d26607e3641039f1c5691603b02f8989fbfcecfa
Cart <cartrigger@gmail.com>
2024-01-16 22:13:31 -0500

    Major update

--- platform-1.18/src/main/java/pm/c7/scout/ScoutNetworking.java
+++
@@ -1,7 +0,0 @@
-package pm.c7.scout;
-
-import net.minecraft.util.Identifier;
-
-public class ScoutNetworking {
-    public static final Identifier ENABLE_SLOTS = new Identifier(Scout.MOD_ID, "enable_slots");
-}
